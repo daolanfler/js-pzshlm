86f7a7e3c572f8eee7b296da42f012faa86ea518
qifang <fangqi3231@cet-electric.com>
2023-06-06 09:56:31 +0800

    chore: update ignore

--- ch02/rollup-example/index.js
+++ ch02/rollup-example/index.js
@@ -28,5 +28,7 @@ export function type(data) {
   return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
 }
 
-const x = Array.from("abc");
-window.x = x;
+export function testArrayFrom() {
+  const x = Array.from("abc");
+  console.log(x);
+}
