cf028931cb7fca31499bee8bc958ab118db57321
qifang <fangqi3231@cet-electric.com>
2023-06-06 15:20:18 +0800

    chore: update

---
+++ ch09-cli/dummy/test-template.js
@@ -0,0 +1,21 @@
+import template from "template_js";
+import { copyTmpl } from "../util/copy.js";
+import path from "path";
+import { getDirName } from "../util/file.js";
+
+const str = `<span><%=name%></span>`;
+const x = template(str, { name: "yan" });
+console.log(x);
+
+const str2 = `
+<span><%if (win) {%> 胜利 <%} else {%> 失败 <%}%></span>
+`;
+
+const x1 = template(str2, { win: true });
+console.log(x1);
+
+const curDirname = getDirName(import.meta.url);
+
+copyTmpl(path.resolve(curDirname, "./a.tmpl"), path.resolve(curDirname, "b"), {
+  win: true,
+});
--- ch09-cli/util/copy.js
+++ ch09-cli/util/copy.js
@@ -1,10 +1,36 @@
-import { copy, mkdirSync, mkdirp } from "fs-extra";
+import fs from "fs-extra";
+import path from "path";
+import template from "template_js";
 
 export async function copyFile(from, to) {
   console.log(from, to);
-  await copy(from, to);
+  await fs.copy(from, to);
 }
 
 export function mkdirSyncGuard(target) {
-  mkdirSync(target, { recursive: true });
+  try {
+    // in case recursive is not supported
+    fs.mkdirSync(target, { recursive: true });
+  } catch (e) {
+    // ensure
+    fs.mkdirpSync(target);
+  }
+}
+
+export async function copyTmpl(from, to, data = {}) {
+  if (path.extname(from) !== ".tmpl") {
+    await copyFile(from, to);
+    return 
+  }
+  const parentPath = path.dirname(to);
+
+  mkdirSyncGuard(parentPath);
+  fs.writeFileSync(to, readTmpl(from, data), { encoding: "utf-8" });
+}
+
+function readTmpl(from, data) {
+  const content = fs.readFileSync(from, {
+    encoding: "utf-8",
+  });
+  return template(content, data);
 }
